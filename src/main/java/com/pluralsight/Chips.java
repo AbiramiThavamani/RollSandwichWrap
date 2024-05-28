eef2854ff055d035774edb961b3fe9b9fec13416
AbiramiThavamani <athavamani@pgh.yearup.org>
2024-05-28 15:52:43 -0400

    added drinks class

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,33 +1,25 @@
 package com.pluralsight;
 
 public class Chips extends OrderItem {
+private  String name;
 
-private final Chips type;
-private final String name;
-
-public enum ChipType{
-    regular,salted,cheese,spicy;
-}
-
-    public Chips(String name, double price, Chips type, String name1) {
+    public Chips(String name, double price) {
         super(name, price);
-        this.type = type;
-        this.name = name1;
+
     }
 
     public double getPrice(){
         return 1.50;
     }
 
-    public String getOrderDetails(){
-        return "Chip:" + type.getName();
-    }
 
     public String getName(){
     return name;
     }
 
-    public Chips getType() {
-        return type;
+    public String getOrderDetails(){
+
+        return "Chip:"+ getName();
     }
+
 }
--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
-public class Drink extends Order {
+public class Drink extends OrderItem {
+
+    private String size;
+    private String name;
+    private double price;
+
+    public Drink(String name, double price, String size, double price1) {
+        super(name, price);
+        this.size = size;
+        this.name = name;
+        this.price = price1;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public String getOrderDetails (){
+        StringBuilder details = new StringBuilder();
+        details.append("Drink:").append(size.getSize()).append("\n");
+        return details.toString();
+    }
 }

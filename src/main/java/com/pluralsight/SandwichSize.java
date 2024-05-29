997766c1c733d376ac307cec15b8a4bd4102035f
AbiramiThavamani <athavamani@pgh.yearup.org>
2024-05-29 11:02:25 -0400

    added sandwich size

--- src/main/java/com/pluralsight/SandwichSize.java
+++ src/main/java/com/pluralsight/SandwichSize.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
-public class SandwichSize {
+public enum SandwichSize {
+
+    FOUR("4\"",5.50),
+    EIGHT("8\"",7.00),
+    TWELVE("12\"",8.50);
+
+    private String inches;
+    private double price;
+
+    SandwichSize(String inches, double price) {
+        this.inches = inches;
+        this.price = price;
+    }
+
+    public String getInches() {
+        return inches;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
